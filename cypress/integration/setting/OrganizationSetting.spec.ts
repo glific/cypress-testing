ebff40a1bf8d369f80a3345bd35fe19ae3f104b3
Kurund Jalmi <kurund.jalmi@webaccessglobal.com>
2020-10-26 13:01:43 +0530

    organization test fixes

--- cypress/integration/setting/OrganizationSetting.spec.ts
+++ cypress/integration/setting/OrganizationSetting.spec.ts
@@ -1,19 +1,21 @@
-describe('Organization Settings', () => {
+describe("Organization Settings", () => {
   beforeEach(function () {
     // login before each test
     cy.login();
-    cy.visit('/settings');
+    cy.visit("/settings");
   });
 
-  it('should navigate to settings list', () => {
-    cy.get('h5').should('contain', 'Settings');
+  it("should navigate to settings list", () => {
+    cy.get("h5").should("contain", "Settings");
   });
 
-  it('should update languages in organization settings', () => {
-    cy.get('[data-testid="organization"]').find('[data-testid="EditIcon"]').click();
-    cy.get('[data-testid="AutocompleteInput"]').first().click().type('Hind');
-    cy.contains('Hindi').click();
+  it("should update languages in organization settings", () => {
+    cy.get('[data-testid="organization"]')
+      .find('[data-testid="EditIcon"]')
+      .click();
+    cy.get('[data-testid="autocomplete-element"]').first().click().type("Kan");
+    cy.contains("Kannada").click();
     cy.get('[data-testid="submitActionButton"]').click();
-    cy.get('div').should('contain', 'Settings edited successfully!');
+    cy.get("div").should("contain", "Settings edited successfully!");
   });
 });
