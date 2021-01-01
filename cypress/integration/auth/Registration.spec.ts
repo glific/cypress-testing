80cc10ce74bb3a6d3fc30ad4934071a2413c250d
DigneshGujarathi <dignesh.gujarathi@webaccessglobal.com>
2021-01-01 19:40:56 +0530

    Added forgot password & registration

---
+++ cypress/integration/auth/Registration.spec.ts
@@ -0,0 +1,25 @@
+describe("Registration page", () => {
+  const phone = Cypress.env("phone");
+  const password = Cypress.env("password");
+
+  it("Load the forgot password page", () => {
+    cy.visit("/registration");
+    cy.get("h4").should("contain", "Create your new account");
+  });
+
+  it("Check validations", () => {
+    cy.contains("CONTINUE").click();
+    cy.get("p").should("contain", "Input required");
+  });
+
+  it("Submit Registration form", () => {
+    cy.get("input[type=text]").type("Test User");
+    cy.get("input[type=tel]").type(phone);
+    cy.get("input[type=password]").type(password);
+    cy.get('[data-testid="SubmitButton"]').click();
+    cy.get("div").should(
+      "contain",
+      "We are unable to register, kindly contact your technical team."
+    );
+  });
+});
