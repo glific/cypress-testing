e06824788adbd45580ce0cb30695320279f845a4
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-01-29 13:29:04 +0530

    added condition to check if recent contact is not simulator

--- cypress/integration/simulator/simulator.spec.ts
+++ cypress/integration/simulator/simulator.spec.ts
@@ -2,13 +2,26 @@ describe("Flow", () => {
     const flow = "test " + +new Date();
 
     beforeEach(function () {
-        // login before each test
+        // login before each testdata-testid="beneficiaryName"
         cy.login();
         cy.visit("/chat");
     });
 
     it("should start simulator", () => {
-        cy.get('[data-testid="simulatorIcon"]').click();
-        cy.get("#simulator");
+        cy.get('[data-testid="beneficiaryName"]').then((body) => {
+            if (body[0].innerText !== "Simulator") {
+                cy.get('[data-testid="simulatorIcon"]').click();
+                cy.get("#simulator");
+            }
+        })
+    });
+    it("should trigger flow", () => {
+        cy.get('[data-testid="beneficiaryName"]').then((body) => {
+            if (body[0].innerText !== "Simulator") {
+                cy.get('[data-testid="simulatorIcon"]').click();
+                cy.get("#simulator");
+            }
+        })
+        cy.get("[data-testid=simulatorInput]").click().wait(500).type("help" + "{enter}");
     });
 });
