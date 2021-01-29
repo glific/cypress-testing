275ff220686fe51d3494bdb203b72e878ebcc7e2
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-01-29 19:06:22 +0530

    formating fixes

--- cypress/integration/simulator/simulator.spec.ts
+++ cypress/integration/simulator/simulator.spec.ts
@@ -1,43 +1,62 @@
 describe("Flow", () => {
-    beforeEach(function () {
-        // login before each
-        cy.login();
-        cy.visit("/chat");
-    });
+  beforeEach(function () {
+    // login before each
+    cy.login();
+    cy.visit("/chat");
+  });
 
-    it("should start simulator", () => {
-        cy.get('[data-testid="beneficiaryName"]').then((body) => {
-            if (body[0].innerText !== "Simulator") {
-                cy.get('[data-testid="simulatorIcon"]').click();
-                cy.get("#simulator");
-            }
-        })
+  it("should start simulator", () => {
+    cy.get('[data-testid="beneficiaryName"]').then((body) => {
+      if (body[0].innerText !== "Simulator") {
+        cy.get('[data-testid="simulatorIcon"]').click();
+        cy.get("#simulator");
+      }
     });
-    it("should trigger help flow", () => {
-        cy.get('[data-testid="beneficiaryName"]').then((body) => {
-            if (body[0].innerText !== "Simulator") {
-                cy.get('[data-testid="simulatorIcon"]').click();
-                cy.get("#simulator");
-            }
-        })
-        const old_msg_count = cy.get('*[class^="Simulator_ReceivedMessage"]').its('length');
-        cy.get("[data-testid=simulatorInput]").click().wait(500).type("help" + "{enter}");
-        cy.get("[data-testid=simulatorInput]").click().wait(500).type("1" + "{enter}");
-        const new_msg_count = cy.get('*[class^="Simulator_ReceivedMessage"]').its('length');
-        expect(new_msg_count).to.not.equal(old_msg_count)
+  });
+  it("should trigger help flow", () => {
+    cy.get('[data-testid="beneficiaryName"]').then((body) => {
+      if (body[0].innerText !== "Simulator") {
+        cy.get('[data-testid="simulatorIcon"]').click();
+        cy.get("#simulator");
+      }
     });
-    it("should trigger activity flow", () => {
-        cy.get('[data-testid="beneficiaryName"]').then((body) => {
-            if (body[0].innerText !== "Simulator") {
-                cy.get('[data-testid="simulatorIcon"]').click();
-                cy.get("#simulator");
-            }
-        })
-        const old_msg_count = cy.get('*[class^="Simulator_ReceivedMessage"]').its('length');
-        cy.get("[data-testid=simulatorInput]").click().wait(500).type("activity" + "{enter}");
-        cy.get("[data-testid=simulatorInput]").click().wait(500).type("1" + "{enter}");
-        const new_msg_count = cy.get('*[class^="Simulator_ReceivedMessage"]').its('length');
-        expect(new_msg_count).to.not.equal(old_msg_count)
+    const old_msg_count = cy
+      .get('*[class^="Simulator_ReceivedMessage"]')
+      .its("length");
+    cy.get("[data-testid=simulatorInput]")
+      .click()
+      .wait(500)
+      .type("help" + "{enter}");
+    cy.get("[data-testid=simulatorInput]")
+      .click()
+      .wait(500)
+      .type("1" + "{enter}");
+    const new_msg_count = cy
+      .get('*[class^="Simulator_ReceivedMessage"]')
+      .its("length");
+    expect(new_msg_count).to.not.equal(old_msg_count);
+  });
+  it("should trigger activity flow", () => {
+    cy.get('[data-testid="beneficiaryName"]').then((body) => {
+      if (body[0].innerText !== "Simulator") {
+        cy.get('[data-testid="simulatorIcon"]').click();
+        cy.get("#simulator");
+      }
     });
-
+    const old_msg_count = cy
+      .get('*[class^="Simulator_ReceivedMessage"]')
+      .its("length");
+    cy.get("[data-testid=simulatorInput]")
+      .click()
+      .wait(500)
+      .type("activity" + "{enter}");
+    cy.get("[data-testid=simulatorInput]")
+      .click()
+      .wait(500)
+      .type("1" + "{enter}");
+    const new_msg_count = cy
+      .get('*[class^="Simulator_ReceivedMessage"]')
+      .its("length");
+    expect(new_msg_count).to.not.equal(old_msg_count);
+  });
 });
