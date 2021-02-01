e50bbae6673786d084898cf315ebb6cd3bf61be7
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-02-01 18:59:14 +0530

    added test tagging a message

--- cypress/integration/message/Message.spec.ts
+++ cypress/integration/message/Message.spec.ts
@@ -12,10 +12,17 @@ describe("Message", () => {
       }
     });
     cy.get("[data-testid=simulatorInput]")
-    .click()
-    .wait(500)
-    .type("hi" + "{enter}");
-    cy.get('[data-testid="messageOptions"]').last().click();
-    cy.contains("Assign tag").click()
+      .click()
+      .wait(500)
+      .type("hi" + "{enter}");
+    const old_tag_count = cy.get('[data-testid="tags"]').last().its("length");
+    cy.get('[data-testid="messageOptions"]').last().wait(500).click();
+    cy.contains("Assign tag").click();
+    cy.get('[title="Open"]').wait(500).click();
+    cy.get(".MuiAutocomplete-popper").wait(500).first().click();
+    cy.get('[title="Close"]').wait(500).click();
+    cy.get('[data-testid="ok-button"]').click().wait(500);
+    const new_tag_count = cy.get('[data-testid="tags"]').last().its("length");
+    expect(new_tag_count).to.not.equal(old_tag_count);
   });
 });
