7a349510b8f665a77de81af74783920094ec912b
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-02-03 17:34:57 +0530

    added test to update staff role

--- cypress/integration/staffmanagement/StaffManagement.spec.ts
+++ cypress/integration/staffmanagement/StaffManagement.spec.ts
@@ -8,4 +8,12 @@ describe("Staff Management", () => {
   it("should load staff management list", () => {
     cy.get("h5").should("contain", "Staff Management");
   });
+
+  it("should change role of staff", () => {
+    cy.get('[data-testid="EditIcon"]').first().click();
+    cy.get('[title="Open"]').first().click();
+    cy.get(".MuiAutocomplete-option").last().click();
+    cy.get('[data-testid="submitActionButton"]').click();
+    cy.contains("User edited successfully!");
+  });
 });
