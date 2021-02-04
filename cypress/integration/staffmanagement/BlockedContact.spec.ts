2a454066712ab47e29936a7635103873eef5d0ec
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-02-04 16:07:01 +0530

    added test to unblock the contact

--- cypress/integration/staffmanagement/BlockedContact.spec.ts
+++ cypress/integration/staffmanagement/BlockedContact.spec.ts
@@ -4,27 +4,37 @@ describe("Blocked Contact", () => {
     cy.login();
   });
 
-  it("should load blocked contacts list", () => {
-  cy.visit("/blocked-contacts");
-    cy.get("h5").should("contain", "Blocked contacts");
-  });
+  // it("should load blocked contacts list", () => {
+  // cy.visit("/blocked-contacts");
+  //   cy.get("h5").should("contain", "Blocked contacts");
+  // });
 
   it("should block a contact", () => {
     cy.visit("/chat");
     
     cy.get('[data-testid="beneficiaryName"]').then((body) => {
+      let contact_name;
       if (body[0].innerText !== "Simulator") {
+        contact_name = body[0].innerText
         cy.get('[data-testid="dropdownIcon"')
         .click(); 
       } else {
         cy.get("#simulator").get('[data-testid="clearIcon"').click()
         cy.get('*[class^="ChatConversation_ChatName"]').eq(1).click()
+        cy.get('[data-testid="beneficiaryName"]').then((body) => {
+        contact_name = body[0].innerText
+        });
         cy.get('[data-testid="dropdownIcon"')
         .click();
       }
       cy.contains("Block Contact").click();
       cy.contains("Confirm").click();
       cy.get("div").should("contain", "Contact blocked successfully");
+      cy.visit("/blocked-contacts");
+      cy.get('[data-testid="searchInput"]').click().type(contact_name);
+      cy.get('[data-testid="additionalButton"]').first().click()
+      cy.contains("Confirm").click();
+      cy.get("div").should("contain", "Contact unblocked successfully");
     });
 
   });
