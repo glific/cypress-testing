864595aad7796c67af860d04b8247b76da599413
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-02-04 18:56:48 +0530

    cleanups

--- cypress/integration/staffmanagement/BlockedContact.spec.ts
+++ cypress/integration/staffmanagement/BlockedContact.spec.ts
@@ -4,15 +4,15 @@ describe("Blocked Contact", () => {
     cy.login();
   });
 
-  // it("should load blocked contacts list", () => {
-  // cy.visit("/blocked-contacts");
-  //   cy.get("h5").should("contain", "Blocked contacts");
-  // });
+  it("should load blocked contacts list", () => {
+  cy.visit("/blocked-contacts");
+    cy.get("h5").should("contain", "Blocked contacts");
+  });
 
   it("should block a contact", () => {
     cy.visit("/chat");
     
-    cy.get('[data-testid="beneficiaryName"]').then((body) => {
+    cy.get('*[class^="ContactBar_ContactInfoContainer"]').find('[data-testid="beneficiaryName"]').then((body) => {
       let contact_name;
       if (body[0].innerText !== "Simulator") {
         contact_name = body[0].innerText
@@ -29,7 +29,7 @@ describe("Blocked Contact", () => {
       }
       cy.contains("Block Contact").click();
       cy.contains("Confirm").click();
-      cy.get("div").should("contain", "Contact blocked successfully");
+      cy.get('*[class^="MuiAlert-message"]').should("contain", "Contact blocked successfully");
       cy.visit("/blocked-contacts");
       cy.get('[data-testid="searchInput"]').click().type(contact_name);
       cy.get('[data-testid="additionalButton"]').first().click()
