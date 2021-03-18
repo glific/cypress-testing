677e389792ed911551d155ca416cf5e7793dd139
Akhilesh Negi <akhilesh.negi@coloredcow.in>
2021-03-18 16:39:46 +0530

    adding contact to collection

--- cypress/integration/roles/staff/chat/AnInitialSetup.spec.ts
+++ cypress/integration/roles/staff/chat/AnInitialSetup.spec.ts
@@ -8,29 +8,37 @@ describe("Staff Management", () => {
     cy.wait(500);
   });
 
-  //   it("should create new collection", () => {
-  //     cy.create_collection(collectionName);
-  //   });
+    it("should create new collection", () => {
+      cy.create_collection(collectionName);
+    });
 
-  it("should assign collection to staff", () => {
+  it("should assign collection to staff and add contact to collection", () => {
     cy.get('[data-testid="tableBody"]').then((body) => {
       cy.contains("NGO Staff")
         .parent()
         .parent()
         .parent()
-        .children().find('[data-testid="EditIcon"]').click({ force: true });
+        .children()
+        .find('[data-testid="EditIcon"]')
+        .click({ force: true });
+      cy.get('[title="Open"]').last().click();
+      cy.get(".MuiAutocomplete-option")
+        .contains(collectionName)
+        .children()
+        .click();
+      cy.get('[title="Close"]').last().click();
+      cy.get('[data-testid="submitActionButton"]').click({ force: true });
+      cy.contains("User edited successfully!");
+      
+      cy.visit("/collection")
+      cy.get("input[name=searchInput]").type(collectionName + "{enter}");
+      cy.get("[data-testid=additionalButton]").first().click();
+      cy.get("[data-testid=autocomplete-element]")
+        .type("Simulator" + "{enter}")
+        .wait(500);
+      cy.get(".MuiAutocomplete-option").first().click();
+      cy.get('[data-testid="ok-button"]').click({ force: true });
+      cy.get("div").should("contain", "1 contact added");
     });
-
-    //   get('[data-testid="EditIcon"]').click({ force: true });
-    // cy.get('[data-testid="tableBody"]').last().click({ force: true });
-    // cy.contains("NGO Staff").;
-    // cy.get('[title="Open"]').last().click();
-    // cy.get(".MuiAutocomplete-option")
-    //   .contains(collectionName)
-    //   .children()
-    //   .click();
-    // cy.get('[title="Close"]').last().click();
-    // cy.get('[data-testid="submitActionButton"]').click({ force: true });
-    // cy.contains("User edited successfully!");
   });
 });
