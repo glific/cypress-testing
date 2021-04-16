242d30e9e5ea1180df5a5e4cec730114fcb5fdbc
kumudini <kumudinikshirsagar2@gmail.com>
2021-04-16 17:18:52 +0530

    added test cases for preview sample messages and attached files in simulator

--- cypress/integration/template/HSMTemplate.spec.ts
+++ cypress/integration/template/HSMTemplate.spec.ts
@@ -1,5 +1,11 @@
 describe("HSM Template", () => {
   const hsmTemplateName = "Sample HSM Templates " + +new Date();
+  const sampleMessage = "This is a sample message";
+  const imageURL = 'https://www.buildquickbots.com/whatsapp/media/sample/jpg/sample01.jpg';
+  const documentURL = 'https://www.buildquickbots.com/whatsapp/media/sample/pdf/sample01.pdf';
+  const videoURL = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4';
+  const audioURL = 'https://actions.google.com/sounds/v1/alarms/bugle_tune.ogg';
+  const stickerURL = 'https://www.buildquickbots.com/whatsapp/media/sample/jpg/sample01.jpg';
 
   beforeEach(function () {
     // login before each test
@@ -70,4 +76,99 @@ describe("HSM Template", () => {
   //   cy.contains("Confirm").click();
   //   cy.get("div").should("contain", "HSM Template deleted successfully");
   // });
+
+  it("should show typed sample message in simulator", () => {
+    cy.get('[data-testid="newItemButton"]').click();
+    cy.wait(1000);
+    cy.get("input[name=label]").click().wait(500).type(hsmTemplateName);
+
+    cy.get(":nth-child(5) > .MuiFormControl-root > [data-testid=outlinedInput]").click().type(sampleMessage).blur({ force: true });
+    cy.get('html').click();
+    cy.wait(1000);
+    cy.get(".Simulator_ReceivedMessage__HGUkF").should("contain", sampleMessage);
+  });
+
+  it("should show attached image with the sample message as caption", () => {
+    cy.get('[data-testid="newItemButton"]').click();
+    cy.wait(1000);
+    
+    cy.get(":nth-child(8) > .MuiFormControl-root").click({ force: true }).type('IMA')
+    cy.contains('IMAGE').click();
+    cy.get(":nth-child(9) > .MuiFormControl-root").click().type(imageURL);
+
+    cy.get(":nth-child(5) > .MuiFormControl-root > [data-testid=outlinedInput]").click().type(sampleMessage).blur({ force: true });
+    cy.get('html').click();
+    cy.wait(5000);
+
+    cy.get('[data-testid=imageMessage] > img').should('have.attr','src', imageURL);
+    cy.get(".Simulator_ReceivedMessage__HGUkF").should("contain", sampleMessage);
+
+  });
+
+  it("should show attached document with the sample message as caption", () => {
+    cy.get('[data-testid="newItemButton"]').click();
+    cy.wait(1000);
+    
+    cy.get(":nth-child(8) > .MuiFormControl-root").click({ force: true }).type('DOC')
+    cy.contains('DOCUMENT').click();
+    cy.get(":nth-child(9) > .MuiFormControl-root").click().type(documentURL);
+
+    cy.get(":nth-child(5) > .MuiFormControl-root > [data-testid=outlinedInput]").click().type(sampleMessage).blur({ force: true });
+    cy.get('html').click();
+    cy.wait(5000);
+
+    cy.get(".ChatMessageType_DocumentText__1j_QQ").should('have.attr', 'href', documentURL);
+    cy.get(".Simulator_ReceivedMessage__HGUkF").should("contain", sampleMessage);
+
+  });
+
+  it("should show attached video with the sample message as caption", () => {
+    cy.get('[data-testid="newItemButton"]').click();
+    cy.wait(1000);
+
+    cy.get(":nth-child(8) > .MuiFormControl-root").click({ force: true }).type('VID')
+    cy.contains('VIDEO').click();
+    cy.get(":nth-child(9) > .MuiFormControl-root").click().type(videoURL).type('{enter}');
+
+    cy.get(":nth-child(5) > .MuiFormControl-root > [data-testid=outlinedInput]").click().type(sampleMessage).blur({ force: true });
+    cy.get('html').click();
+    cy.wait(1000);
+    cy.get('[data-testid=videoMessage] > .ChatMessageType_Image__1bMAz');
+    
+    cy.get(".Simulator_ReceivedMessage__HGUkF").should("contain", sampleMessage);
+    
+  });
+
+  it("should show attached audio", () => {
+    cy.get('[data-testid="newItemButton"]').click();
+    cy.wait(1000);
+
+
+    cy.get(":nth-child(8) > .MuiFormControl-root").click({ force: true }).type('AUD')
+    cy.contains('AUDIO').click();
+    cy.get(":nth-child(9) > .MuiFormControl-root").click().type(audioURL);
+    cy.get('html').click();
+    cy.wait(5000);
+
+    cy.get('[data-testid=audioMessage] > source');
+    cy.should('have.attr', 'src', audioURL);
+  });
+
+  it("should show attached sticker", () => {
+    cy.get('[data-testid="newItemButton"]').click();
+    cy.wait(1000);
+
+    cy.get(":nth-child(8) > .MuiFormControl-root").click({ force: true }).type('STI')
+    cy.contains('STICKER').click();
+    cy.get(":nth-child(9) > .MuiFormControl-root").click().type(stickerURL);
+    cy.get('html').click();
+    cy.wait(5000);
+
+    cy.get('[data-testid=stickerMessage]');
+    cy.should('have.attr', 'src', stickerURL);
+    
+  });
+
 });
+
+
