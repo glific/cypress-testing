13fb38fbe5cda3992386a2fa89977377971dfd4e
mdshamoon <mohdshamoon841@gmail.com>
2022-12-07 15:58:51 +0530

    fixed test cases

--- cypress/integration/auth/Registration.spec.ts
+++ cypress/integration/auth/Registration.spec.ts
@@ -4,27 +4,20 @@ describe('Registration page', () => {
 
   it('Load the forgot password page', () => {
     cy.visit('/registration');
-    cy.get('h4').should('contain', 'Create your new account');
+    cy.get('span').should('contain', 'Register with ');
   });
 
-  it('Check validations', () => {
-    cy.get('[data-testid="SubmitButton"]').click();
-    cy.get('p').should('contain', 'Input required');
+  it('Register button should be disabled by default', () => {
+    cy.get('[data-testid="SubmitButton"]').should('have.attr', 'disabled');
   });
 
-  it('Check error on Registration form', () => {
+  it('Should be able to enter the values in form', () => {
     cy.get('input[type=text]').type('Test User');
     cy.get('input[type=tel]').type(phone);
     cy.get('input[type=password]').type(password);
-    cy.get('[data-testid="SubmitButton"]').click();
-    cy.get('div').should(
-      'contain',
-      'We are unable to register, kindly contact your technical team.'
-    );
-  });
 
-  // it("Redirect to login form", () => {
-  //   cy.contains("LOGIN TO GLIFIC").click();
-  //   cy.get("h4").should("contain", "Login to your account");
-  // });
+    cy.get('input[type=text]').should('have.attr', 'value', 'Test User');
+    cy.get('input[type=tel]').should('have.attr', 'value', '+91' + phone);
+    cy.get('input[type=password]').should('have.attr', 'value', password);
+  });
 });
