33ad6fb380c8b6bc6317fb35a765146099f982ea
Kurund Jalmi <kurund.jalmi@gmail.com>
2023-01-11 00:01:27 +0000

    test cleanup

--- cypress/e2e/auth/ForgotPassword.spec.ts
+++ cypress/e2e/auth/ForgotPassword.spec.ts
@@ -1,19 +1,20 @@
 describe('Forgot password page', () => {
   const phone = Cypress.env('phone');
 
-  it('Load the forgot password page', () => {
+  beforeEach(function () {
     cy.visit('/resetpassword-phone');
+  });
+
+  it('Load the forgot password page', () => {
     cy.get('h4').should('contain', 'Reset your password');
   });
 
   it('Check validations', () => {
-    cy.visit('/resetpassword-phone');
     cy.get('[data-testid="SubmitButton"]').click();
     cy.get('p').should('contain', 'Input required');
   });
 
   it('Successful otp send', () => {
-    cy.visit('/resetpassword-phone');
     cy.get('input[type=tel]').type(phone);
     cy.get('[data-testid="SubmitButton"]').click();
     cy.get('div').should('contain', 'Please confirm the OTP received at your WhatsApp number.');
--- cypress/e2e/auth/Registration.spec.ts
+++ cypress/e2e/auth/Registration.spec.ts
@@ -2,18 +2,19 @@ describe('Registration page', () => {
   const phone = Cypress.env('phone');
   const password = Cypress.env('password');
 
-  it('Load the forgot password page', () => {
+  beforeEach(function () {
     cy.visit('/registration');
+  });
+
+  it('Load the forgot password page', () => {
     cy.get('span').should('contain', 'Register with ');
   });
 
   it('Register button should be disabled by default', () => {
-    cy.visit('/registration');
     cy.get('[data-testid="SubmitButton"]').should('have.attr', 'disabled');
   });
 
   it('Should be able to enter the values in form', () => {
-    cy.visit('/registration');
     cy.get('input[type=text]').type('Test User');
     cy.get('input[type=tel]').type(phone);
     cy.get('input[type=password]').type(password);
