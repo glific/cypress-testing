e68d2018a5b7927ce93182cd8ac8f97da0dcc7a3
mdshamoon <mohdshamoon841@gmail.com>
2023-02-24 19:31:28 +0530

    fixed test cases

--- cypress/e2e/roles/staff/chat/Chat.spec.ts
+++ cypress/e2e/roles/staff/chat/Chat.spec.ts
@@ -155,19 +155,19 @@ describe('Role - Staff - Chats', () => {
     cy.get('div[data-testid="timerContainer"]').then((param) => {
       if (parseInt(param[0].innerText) > 10) {
         cy.sessionTimer(
-          'Timer_TimerNormal',
+          '_TimerNormal',
           'Session window is open to message this contact. Learn more about the WhatsApp session window here.'
         );
       }
       if (parseInt(param[0].innerText) > 0 && parseInt(param[0].innerText) < 5) {
         cy.sessionTimer(
-          'Timer_TimerApproachEnd',
+          '_TimerApproachEnd',
           'Your message window is about to expire! Learn more about the WhatsApp session window here.'
         );
       }
       if (parseInt(param[0].innerText) == 0) {
         cy.sessionTimer(
-          'Timer_TimerEnd',
+          '_TimerEnd',
           'Session message window has expired! You can only send a template message now. Learn more about the WhatsApp session window here.'
         );
       }
