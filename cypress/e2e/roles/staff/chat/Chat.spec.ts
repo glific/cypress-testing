5fd3b9cdf9648ead6a19520f46c9ec5b44cd92e2
akanshaaaa19 <asakhre2002@gmail.com>
2024-06-10 21:21:28 +0530

    fixed chat test case

--- cypress/e2e/roles/staff/chat/Chat.spec.ts
+++ cypress/e2e/roles/staff/chat/Chat.spec.ts
@@ -38,7 +38,7 @@ describe('Role - Staff - Chats', () => {
   it('should send the speed send', () => {
     cy.get('[data-testid="shortcut-open-button"]').click().wait(500);
     cy.wait(500);
-    cy.get('[data-testid="shortcutButton"]').eq(2).click().wait(500);
+    cy.get('[data-testid="shortcutButton"]').contains('Speed sends').click().wait(500);
     cy.get('[data-testid="templateItem"] :first').click();
     cy.get('[data-testid="sendButton"]').click();
     // TODOS: Due to some wierd subscription related issue in the test run below assertion is failing
@@ -47,48 +47,41 @@ describe('Role - Staff - Chats', () => {
   });
 
   it('should send the templates', () => {
-    cy.visit('/chat');
     cy.get('[data-testid="shortcut-open-button"]').click().wait(500);
     cy.get('[data-testid="shortcutButton"]').then((shortcutButton) => {
       // check if we have both the templates and speed send button
-      if (shortcutButton.length === 2) {
+      if (shortcutButton.length === 2 && shortcutButton.eq(0).contents().text() === 'Templates') {
         cy.get('[data-testid="shortcutButton"]')
           .contains('Templates')
           .eq(0)
-          .then((body) => {
-            if (body.length > 0) {
-              cy.get('[data-testid="shortcutButton"]')
-                .contains('Templates')
-                .eq(0)
-                .click({ multiple: true, force: true });
-              cy.get('.ChatInput_ChatSearchBar__zM149 .MuiInputBase-input')
-                .click({ multiple: true, force: true })
-                .type('attached bill');
-              cy.get('div:nth-child(1) > [data-testid="templateItem"]').then((param) => {
-                if (param.length > 0) {
-                  cy.get('div:nth-child(1) > [data-testid="templateItem"]').click();
-                  cy.get('[data-testid=AutocompleteInput]').click().type('ABC');
-                  cy.get('[data-testid="ok-button"]').click();
+          .click({ multiple: true, force: true });
+        cy.get('.ChatInput_ChatSearchBar__zM149 .MuiInputBase-input')
+          .click({ multiple: true, force: true })
+          .type('attached bill');
+        cy.get('div:nth-child(1) > [data-testid="templateItem"]').then((param) => {
+          if (param.length > 0) {
+            cy.get('div:nth-child(1) > [data-testid="templateItem"]').click();
+            cy.get('[data-testid=AutocompleteInput]').click().type('ABC');
+            cy.get('[data-testid="ok-button"]').click();
 
-                  // check if the template is showing on screen after send
-                  cy.get('[data-testid="editor"]').then((text) => {
-                    cy.get('[data-testid="sendButton"]').click();
-                    cy.get('.ChatMessage_Content__1CvXE')
-                      .last()
-                      .then((msgContent) => {
-                        cy.get('div').should('contain', text[0].innerText);
-                      });
-                  });
-                }
-              });
-            }
-          });
+            // check if the template is showing on screen after send
+            cy.get('[data-testid="editor"]').then((text) => {
+              cy.get('[data-testid="sendButton"]').click();
+              cy.get('.ChatMessage_Content__1CvXE')
+                .last()
+                .then((msgContent) => {
+                  cy.get('div').should('contain', text[0].innerText);
+                });
+            });
+          }
+        });
+      } else {
+        cy.get('[data-testid="shortcut-open-button"]').click().wait(500);
       }
     });
   });
 
   it('should send add to speed send', () => {
-    cy.visit('/chat');
     cy.get('[data-testid="message"]:last()').find('svg').click({ multiple: true, force: true });
     cy.contains('Add to speed sends').click({ force: true });
     // check input field validation
