9d23d3f83315910b12c4dcdc6317bb0623485775
Akansha Sakhre <asakhre2002@gmail.com>
2024-07-09 10:33:09 +0530

    minor fix

--- cypress/e2e/roles/staff/collection/Collection.spec.ts
+++ cypress/e2e/roles/staff/collection/Collection.spec.ts
@@ -37,7 +37,7 @@ describe('Role - Staff - Collection', () => {
     cy.get('input[name=searchInput]').type(collectionName + '{enter}');
     cy.get('[data-testid=view]').click();
     cy.wait(1000);
-    cy.get('[type="checkbox"]').check();
+    cy.get('[type="checkbox"]').check({ force: true });
     if (cy.get('[data-testid="deleteBtn"]')) {
       cy.get('[data-testid="deleteBtn"]').first().click({ force: true });
       cy.get('[data-testid="ok-button"]').click({ force: true });
